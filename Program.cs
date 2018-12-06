b9b5ca66a5cde2c1d524de8492470542ad55070a
pawelros <rosiu@outlook.com>
2018-12-06 22:49:04 +0100

    Add dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lets_encrypt_application_gateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
