b152013af13e446fe3d1cea4a152df0903500ac3
pawelros <rosiu@outlook.com>
2018-12-06 23:27:22 +0100

    Add logging

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace lets_encrypt_application_gateway
 {
@@ -6,7 +9,25 @@ namespace lets_encrypt_application_gateway
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("log/lets-encrypt-application-gateway.log")
+                .CreateLogger();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Hello World!");
+
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddSerilog())
+                .AddTransient<Program>();
         }
     }
 }
